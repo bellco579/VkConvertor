6a9fc768a6e5bbba27040c269e4e001fd060390c
bellco <bellco579@gmail.com>
2020-05-16 03:29:24 +0500

    del token

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import vk_requests
 import requests
 import vk_api
 
-token = '195dbce1b5211621a639fb7d9d82c9e0eb7b9051b2907d35649669a1dbb9638f54f9aeb3c8a40c6fd4286'
+token = ''
 vk_session = vk_api.VkApi(token=token)
 import vk
 
