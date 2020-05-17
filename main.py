eaa04f7ed60169e745edddcda1cb95cde18b1777
bellco <bellco579@gmail.com>
2020-05-17 13:31:18 +0500

    bug fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import vk_requests
 import requests
 import vk_api
 
-token = '195dbce1b5211621a639fb7d9d82c9e0eb7b9051b2907d35649669a1dbb9638f54f9aeb3c8a40c6fd4286'
+token = ''
 vk_session = vk_api.VkApi(token=token)
 session = vk.Session()
 api = vk.API(session, v=5.8)
